d5100934e6600181cea41938c806b0ca68e4a129
Daniel-Prizer <danielp24@ru.is>
2025-07-22 19:40:58 +0000

    attempo to fix static files again

--- CPS_Battles/settings.py
+++ CPS_Battles/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR.parent, "static"),
+    str(BASE_DIR.parent / "static"),
 ]
 
 
